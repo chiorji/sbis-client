b7f4a4cdcc6eaa3ccdc9081da136f62900b86dcb
Chigbogu Orji <brightorji60@gmail.com>
2021-05-03 14:40:26 +0100

    Feat; add academics route

--- client/src/request/links.js
+++ client/src/request/links.js
@@ -2,6 +2,7 @@ export const links = [
   {id: 'about', label: 'About', to: '/about'},
   {id: 'portal', label: 'Portal', to: '/portal'},
   {id: 'contact', label: 'Contact', to: '/#contact'},
+  {id: 'academics', label: 'Academics', to: '/academics'},
   {id: 'gallery', label: 'Gallery', to: '/gallery'}
 ];
 
