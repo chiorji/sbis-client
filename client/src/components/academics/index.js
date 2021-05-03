d46f532caa8543e90a787716a3c8e642a9da7d4f
Chigbogu Orji <brightorji60@gmail.com>
2021-05-03 17:30:53 +0100

    Refactor: add footer component

--- client/src/components/academics/index.js
+++ client/src/components/academics/index.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Accordion from './Accordion';
 import makeStyles from '@material-ui/styles/makeStyles';
+import Footer from '../Footer';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,14 +16,17 @@ const useStyles = makeStyles(theme => ({
 const Academics = () => {
   const {root} = useStyles();
   return (
-    <Box className={root}>
-      <Container>
-        <Typography variant="h4">Academics</Typography>
-      </Container>
-      <Container>
-        <Accordion />
-      </Container>
-    </Box>
+    <>
+      <Box className={root}>
+        <Container>
+          <Typography variant="h4">Academics</Typography>
+        </Container>
+        <Container>
+          <Accordion />
+        </Container>
+      </Box>
+      <Footer />
+    </>
   );
 };
 
