755e612ed89e9ceff2cf46c77e411e709870f045
Chigbogu Orji <brightorji60@gmail.com>
2021-05-07 23:19:57 +0100

    Refactor: reduce list

--- client/src/components/staff/listing.js
+++ client/src/components/staff/listing.js
@@ -1,12 +1,10 @@
 import avatar from '../../assets/img/2.jpg';
 
 export const listing = [
-  {avatar: avatar, name: 'Principal', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit laudantium enim iure magni non recusandae maiores harum? Iste laboriosam quam voluptatum.'},
-  {avatar: avatar, name: 'Head Mistress', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit laudantium enim iure magni non recusandae maiores harum? Iste laboriosam quam voluptatum.'},
-  {avatar: avatar, name: 'Form Teacher', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit laudantium enim iure magni non recusandae maiores harum? Iste laboriosam quam voluptatum.'},
-  {avatar: avatar, name: 'Form Teacher', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit laudantium enim iure magni non recusandae maiores harum? Iste laboriosam quam voluptatum.'},
-  {avatar: avatar, name: 'Form Teacher', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit laudantium enim iure magni non recusandae maiores harum? Iste laboriosam quam voluptatum.'},
-  {avatar: avatar, name: 'Form Teacher', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit laudantium enim iure magni non recusandae maiores harum? Iste laboriosam quam voluptatum.'},
-  {avatar: avatar, name: 'Form Teacher', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit laudantium enim iure magni non recusandae maiores harum? Iste laboriosam quam voluptatum.'},
-  {avatar: avatar, name: 'Form Teacher', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit laudantium enim iure magni non recusandae maiores harum? Iste laboriosam quam voluptatum.'}
+  {avatar: avatar, name: 'Principal', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+  {avatar: avatar, name: 'Head Mistress', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+  {avatar: avatar, name: 'Form Teacher', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+  {avatar: avatar, name: 'Form Teacher', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+  {avatar: avatar, name: 'Form Teacher', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+  {avatar: avatar, name: 'Form Teacher', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
 ];
