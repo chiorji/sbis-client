2068cbb4520e166cf4083ef50a57b8cc2c6cb354
Chigbogu Orji <brightorji60@gmail.com>
2021-05-13 12:10:35 +0100

    chore: bootstrap component/pages/

---
+++ client/src/pages/staff/ResultEntry.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+const ResultEntry = () => {
+  return (
+    <Box width={1} component="section">
+      <Typography variant="h4">Result Entry</Typography>
+    </Box>
+  );
+};
+
+
+
+export default ResultEntry;
