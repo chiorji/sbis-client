cfd818f77f27ccf380d16b7858606dc8682a6f19
Chigbogu Orji <brightorji60@gmail.com>
2021-05-14 12:07:47 +0100

    chore: states locally instead of making API calls blocked by CORS

--- client/src/store/staff/fetchStatesSaga.js
+++ client/src/store/staff/fetchStatesSaga.js
@@ -1,20 +1,14 @@
-import axios from 'axios';
-import {takeLatest, call, put, cancel} from 'redux-saga/effects';
-import endpoints from '../../request/endpoints';
+import {takeLatest,  put, cancel} from 'redux-saga/effects';
 import * as actions from './actions';
 import types from './constants';
+import states from '../../utils/states.json';
 
 function* fetchStates (){
   try {
-    const states = yield call(axios, endpoints.fetchStates());
-    yield put(actions.fetchStatesSuccess(states.data));
+    yield put(actions.fetchStatesSuccess(states));
   } catch (error) {
     yield(cancel());
-    if (error.response?.data) {
-      yield put(actions.fetchStatesFailure(error.response?.data.message));
-    } else {
-      yield put(actions.fetchStatesFailure(error.message));
-    }
+    yield put(actions.fetchStatesFailure('Error fetching states'));
   }
 }
 
