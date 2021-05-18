9110c40c9e35bbcbb2367723dc183d3cd6bbfdc2
Chigbogu Orji <brightorji60@gmail.com>
2021-05-18 01:14:52 +0100

    Feat: bootstrap subject listing component

---
+++ client/src/components/Subjects/SubjectsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const SubjectsList = () => {
+  return (
+    <>
+      <Typography variant="h4" gutterBottom>Subjects List</Typography>
+      <Grid container>
+      </Grid>
+    </>
+  );
+};
+
+export default SubjectsList;
