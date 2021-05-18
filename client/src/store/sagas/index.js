18338da90a75eda8c19efac6d57de3229e088418
Chigbogu Orji <brightorji60@gmail.com>
2021-05-18 23:58:13 +0100

    Chore: merge file

--- client/src/store/sagas/index.js
+++ client/src/store/sagas/index.js
@@ -2,13 +2,12 @@
 import {all, call, spawn} from 'redux-saga/effects';
 import states from '../staff/fetchStatesSaga';
 import lgas from '../staff/fetchLgasSaga';
-import regStudent from '../staff/RegStudentSaga';
 import students from '../staff/studentsSaga';
 import admin from '../staff/adminSaga';
 import subjects from '../staff/subjectsSaga'
 
 export default function* () {
-  const sagas = [states, lgas, regStudent, students, admin, subjects];
+  const sagas = [states, lgas, students, admin, subjects];
 
   yield all(sagas.map(saga =>
     spawn(function* () {
