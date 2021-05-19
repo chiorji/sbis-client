5169fda884c5a2c0fdccdd364bb0a23493322418
Chigbogu Orji <brightorji60@gmail.com>
2021-05-19 12:55:13 +0100

    Feat: subject listing

--- client/src/store/staff/fetchStatesSaga.js
+++ client/src/store/staff/fetchStatesSaga.js
@@ -2,6 +2,7 @@ import {takeLatest,  put, cancel} from 'redux-saga/effects';
 import * as actions from './actions';
 import types from './constants';
 import states from '../../utils/states.json';
+import _lg from '../../utils/lgas.json';
 
 function* fetchStates (){
   try {
@@ -12,7 +13,19 @@ function* fetchStates (){
   }
 }
 
+
+function* fetchLgas({payload}) {
+  try {
+    let lg = yield _lg.filter(lga => lga.state === payload).map(l => l.lgas)[0];
+    yield put(actions.fetchLgaSuccess(lg));
+  } catch (error) {
+    yield(cancel());
+    yield put(actions.fetchLgaFailure('Error getting LGAs'));
+  }
+}
+
 function* watcher() {
+  yield takeLatest(types.FETCH_LGAS, fetchLgas);
   yield takeLatest(types.FETCH_STATES, fetchStates);
 }
 
