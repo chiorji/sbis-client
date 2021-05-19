3a24784915cb1a12bff8f88527f48659ddbe6a72
Chigbogu Orji <brightorji60@gmail.com>
2021-05-19 19:51:21 +0100

    Feat: subject listing

--- client/src/components/Subjects/SubjectsList.js
+++ client/src/components/Subjects/SubjectsList.js
@@ -1,15 +1,63 @@
+/* eslint-disable no-unused-vars, no-console */
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import {connect} from 'react-redux';
+import MaterialTable from 'material-table';
+import EditIcon from '@material-ui/icons/Edit';
+import SearchIcon from '@material-ui/icons/Search';
+import ClearIcon from '@material-ui/icons/Clear';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+// import Button from '@material-ui/core/Button';
+// import TextField from '@material-ui/core/TextField';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+import subjectsDummyData from '../../utils/subjectList.json';
 
-const SubjectsList = () => {
+const SubjectsList = ({subjects}) => {
   return (
-    <>
-      <Typography variant="h4" gutterBottom>Subjects List</Typography>
-      <Grid container>
-      </Grid>
-    </>
+    <MaterialTable
+      title="Subject List"
+      columns={[
+        {title: 'Name', field: 'name', editable: 'onUpdate'},
+        {title: 'Code', field: 'code',editable: 'never'},
+        {title: 'Teacher', field: 'teacher', editable: 'onUpdate'},
+        {title: 'Category', field: 'category', editable: 'onUpdate'}
+      ]}
+      data={subjects.lenght > 0 ? subjects : subjectsDummyData}
+      options={{
+        search:             false,
+        sorting:            false,
+        actionsColumnIndex: -1,
+        paging:             false
+      }}
+      actions={[
+        {
+          icon:    EditIcon,
+          tooltip: 'Edit',
+          onClick: (event, rowData) => alert('You edited ' + rowData.name)
+        }
+      ]}
+      icons={{
+        Search:       SearchIcon,
+        ResetSearch:  ClearIcon,
+        SortArrow:    ArrowDropUpIcon,
+        PreviousPage: ArrowLeftIcon,
+        NextPage:     ArrowRightIcon,
+        FirstPage:    SkipPreviousIcon,
+        LastPage:     SkipNextIcon
+      }}
+    />
   );
 };
 
-export default SubjectsList;
+const mapState = ({staff}) => ({
+  subjects: staff.subjects
+});
+
+export default connect(mapState)(SubjectsList);
