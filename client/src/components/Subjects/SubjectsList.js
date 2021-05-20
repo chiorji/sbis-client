6452cc0617fc5eb18eb1624d2fdd3cce5d6d1dd8
Chigbogu Orji <brightorji60@gmail.com>
2021-05-20 01:28:53 +0100

    Feat: Edit row data

--- client/src/components/Subjects/SubjectsList.js
+++ client/src/components/Subjects/SubjectsList.js
@@ -1,56 +1,87 @@
 /* eslint-disable no-unused-vars, no-console */
-import React from 'react';
+import React, {useCallback} from 'react';
 import {connect} from 'react-redux';
 import MaterialTable from 'material-table';
 import EditIcon from '@material-ui/icons/Edit';
-import SearchIcon from '@material-ui/icons/Search';
-import ClearIcon from '@material-ui/icons/Clear';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
-// import Button from '@material-ui/core/Button';
-// import TextField from '@material-ui/core/TextField';
-// import Dialog from '@material-ui/core/Dialog';
-// import DialogActions from '@material-ui/core/DialogActions';
-// import DialogContent from '@material-ui/core/DialogContent';
-// import DialogContentText from '@material-ui/core/DialogContentText';
-// import DialogTitle from '@material-ui/core/DialogTitle';
+import DoneIcon from '@material-ui/icons/Done';
+import CancelIcon from '@material-ui/icons/Cancel';
 import subjectsDummyData from '../../utils/subjectList.json';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import {updateSubject} from '../../store/staff/actions';
 
-const SubjectsList = ({subjects}) => {
+const SubjectsList = ({subjects, update}) => {
+
+  const updateSub = (data) => {
+    return update(data);
+  };
+
   return (
     <MaterialTable
       title="Subject List"
+      editable={{
+        onRowUpdate: (newData) => {
+          new Promise((resolve, reject) => {
+            setTimeout(() => {
+              try {
+                updateSub(newData);
+                resolve();
+              } catch (error) {
+                reject();
+              }
+            }, 50);
+          });
+        }
+      }}
       columns={[
-        {title: 'Name', field: 'name', editable: 'onUpdate'},
-        {title: 'Code', field: 'code',editable: 'never'},
-        {title: 'Teacher', field: 'teacher', editable: 'onUpdate'},
-        {title: 'Category', field: 'category', editable: 'onUpdate'}
+        {title: 'Name', field: 'name', editable: 'always'},
+        {title: 'Code', field: 'code', editable: 'always'},
+        {
+          title:         'Teacher',
+          field:         'teacher',
+          editable:      'always',
+          editComponent: props => (
+            <TextField
+              type="text"
+              select
+              value={props.value}
+              onChange={e => props.onChange(e.target.value)}
+            > {/* replace with staff name list */}
+              {['Henry', 'Bright', 'Michael', 'Joy'].map(value => (
+                <MenuItem key={value} value={value}>{value}</MenuItem>
+              ))}
+            </TextField>
+          )},
+        {
+          title:         'Category',
+          field:         'category',
+          editable:      'always',
+          editComponent: props => (
+            <TextField
+              type="text"
+              select
+              value={props.value}
+              onChange={e => props.onChange(e.target.value)}
+            >
+              {['General', 'Sciences', 'Arts', 'Finance'].map(value => (
+                <MenuItem key={value} value={value}>{value}</MenuItem>
+              ))}
+            </TextField>
+          )
+        }
       ]}
       data={subjects.lenght > 0 ? subjects : subjectsDummyData}
       options={{
         search:             false,
         sorting:            false,
         actionsColumnIndex: -1,
-        paging:             false
+        paging:             false,
+        draggable:          false
       }}
-      actions={[
-        {
-          icon:    EditIcon,
-          tooltip: 'Edit',
-          onClick: (event, rowData) => alert('You edited ' + rowData.name)
-        }
-      ]}
       icons={{
-        Search:       SearchIcon,
-        ResetSearch:  ClearIcon,
-        SortArrow:    ArrowDropUpIcon,
-        PreviousPage: ArrowLeftIcon,
-        NextPage:     ArrowRightIcon,
-        FirstPage:    SkipPreviousIcon,
-        LastPage:     SkipNextIcon
+        Edit:  EditIcon,
+        Check: DoneIcon,
+        Clear: CancelIcon
       }}
     />
   );
@@ -60,4 +91,8 @@ const mapState = ({staff}) => ({
   subjects: staff.subjects
 });
 
-export default connect(mapState)(SubjectsList);
+const mapDispatch = (dispatch) => ({
+  update: (payload) => dispatch(updateSubject(payload))
+});
+
+export default connect(mapState, mapDispatch)(SubjectsList);
