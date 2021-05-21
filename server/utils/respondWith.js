8319626d225f97008773df8668c6fc39c2fe1e70
Chigbogu Orji <brightorji60@gmail.com>
2021-05-21 04:01:42 +0100

    Chore: format response data, and custom errors

---
+++ server/utils/respondWith.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {*} code
+ * @param {*} msg
+ * @param {*} success
+ * @param {*} data
+ */
+const respondWith = (code, msg, success, data) => {
+  return {
+    status:  typeof (code) !== 'number' ? 200 : code,
+    message: msg ? msg.charAt(0).toUpperCase() + msg.slice(1) : 'Successful',
+    success: typeof (success) === 'boolean' ? success : false,
+    data:    data !== null ? data : [] // never return null
+  };
+};
+
+module.exports = respondWith;
