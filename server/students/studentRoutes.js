fbaba0edcce3f9bfcb367ceef113f96343c38112
Chigbogu Orji <brightorji60@gmail.com>
2021-05-22 10:40:57 +0100

    Feat: Add student admission validation

--- server/students/studentRoutes.js
+++ server/students/studentRoutes.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { admission } = require('./studentMiddleware');
+const ctl = require('./studentController');
+
+router.post('/register', admission, ctl.admit);
 
 module.exports = router;
