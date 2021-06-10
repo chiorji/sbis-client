0cbb418ada41c42c66d91758cea1f37af36be8b5
Chigbogu Orji <brightorji60@gmail.com>
2021-06-10 13:50:50 +0100

    Style: Add more table options

--- src/Components/Staff/StaffListing.js
+++ src/Components/Staff/StaffListing.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import Typography from '@material-ui/core/Typography';
 import MaterialTable from 'material-table';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import SearchIcon from '@material-ui/icons/Search';
@@ -18,39 +19,46 @@ const StaffListing = ({ staff, isLoading, fetchStaffList }) => {
   }, [fetchStaffList]);
 
   return (
-    <MaterialTable
-      title="Staff List"
-      columns={[
-        { title: 'First Name', field: 'first_name' },
-        { title: 'Last Name', field: 'last_name' },
-        { title: 'Form Class', field: 'form_class' },
-        { title: 'id', field: 'id' }
-      ]}
-      data={staff}
-      options={{
-        search:             true,
-        sorting:            true,
-        actionsColumnIndex: -1
-      }}
-      actions={[
-        {
-          icon:    VisibilityIcon,
-          tooltip: 'View',
-          onClick: (event, rowData) => alert('You viewed ' + rowData.id)
-        }
-      ]}
-      icons={{
-        Search:       SearchIcon,
-        ResetSearch:  ClearIcon,
-        SortArrow:    ArrowDropUpIcon,
-        PreviousPage: ArrowLeftIcon,
-        NextPage:     ArrowRightIcon,
-        FirstPage:    SkipPreviousIcon,
-        LastPage:     SkipNextIcon
-      }}
-      tableLayout="fixed"
-      isLoading={isLoading}
-    />
+    <>
+      <Typography variant="h4" gutterBottom>Staff member list</Typography>
+      <MaterialTable
+        columns={[
+          { title: 'First Name', field: 'first_name' },
+          { title: 'Last Name', field: 'last_name' },
+          { title: 'Gender', field: 'gender' },
+          { title: 'Form Class', field: 'form_class' },
+          { title: 'Email', field: 'email' }
+        ]}
+        data={staff}
+        options={{
+          search:              true,
+          sorting:             true,
+          actionsColumnIndex:  -1,
+          toolbar:             false,
+          emptyRowsWhenPaging: false,
+          pageSize:            10,
+          pageSizeOptions:     [10, 20, 30]
+        }}
+        actions={[
+          {
+            icon:    VisibilityIcon,
+            tooltip: 'View',
+            onClick: (event, rowData) => alert('You viewed ' + rowData.first_name)
+          }
+        ]}
+        icons={{
+          Search:       SearchIcon,
+          ResetSearch:  ClearIcon,
+          SortArrow:    ArrowDropUpIcon,
+          PreviousPage: ArrowLeftIcon,
+          NextPage:     ArrowRightIcon,
+          FirstPage:    SkipPreviousIcon,
+          LastPage:     SkipNextIcon
+        }}
+        tableLayout="fixed"
+        isLoading={isLoading}
+      />
+    </>
   );
 };
 
