62a406bc498de18878a4a0a8cd87a84b4ce6dc1f
Chigbogu Orji <brightorji60@gmail.com>
2021-06-15 23:03:04 +0100

    Style: Replace material-table with data-grid The replacement was caused by material-table kinda not being responsive

--- src/Components/Staff/StaffListing.js
+++ src/Components/Staff/StaffListing.js
@@ -1,63 +1,86 @@
+/* eslint-disable no-console */
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
-import MaterialTable from 'material-table';
-import VisibilityIcon from '@material-ui/icons/Visibility';
-import SearchIcon from '@material-ui/icons/Search';
-import ClearIcon from '@material-ui/icons/Clear';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
+import { DataGrid } from '@material-ui/data-grid';
+import { makeStyles } from '@material-ui/styles';
 import { fetchStaffList } from '../../store/staff/actions';
 
-const StaffListing = ({ staff, isLoading, fetchStaffList }) => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& .list-header': {
+      backgroundColor: theme.palette.primary.light,
+      color:           theme.palette.grey[300]
+    }
+  }
+}));
 
+const StaffListing = ({ staff, isLoading, fetchStaffList }) => {
+  const { root } = useStyles();
   useEffect(() => {
     fetchStaffList();
-  }, [fetchStaffList]);
+  }, [ fetchStaffList ]);
+
+  const columns = [
+    {
+      headerName:      'First Name',
+      field:           'first_name',
+      width:           200,
+      id:              1,
+      headerClassName: 'list-header'
+    },
+    {
+      headerName:
+        'Last Name',
+      field:           'last_name',
+      width:           200,
+      id:              2,
+      headerClassName: 'list-header'
+    },
+    {
+      headerName:
+        'Gender',
+      field:           'gender',
+      width:           200,
+      id:              3,
+      headerClassName: 'list-header'
+    },
+    {
+      headerName:      'Form Class',
+      field:           'form_class',
+      width:           200,
+      id:              4,
+      headerClassName: 'list-header'
+    },
+    {
+      headerName:
+        'Email',
+      field:           'email',
+      width:           200,
+      flex:            1,
+      id:              5,
+      headerClassName: 'list-header'
+    }
+  ];
 
   return (
     <>
       <Typography variant="h4" gutterBottom>Staff member list</Typography>
-      <MaterialTable
-        columns={[
-          { title: 'First Name', field: 'first_name' },
-          { title: 'Last Name', field: 'last_name' },
-          { title: 'Gender', field: 'gender' },
-          { title: 'Form Class', field: 'form_class' },
-          { title: 'Email', field: 'email' }
-        ]}
-        data={staff}
-        options={{
-          search:              true,
-          sorting:             true,
-          actionsColumnIndex:  -1,
-          toolbar:             false,
-          emptyRowsWhenPaging: false,
-          pageSize:            10,
-          pageSizeOptions:     [10, 20, 30]
-        }}
-        actions={[
-          {
-            icon:    VisibilityIcon,
-            tooltip: 'View',
-            onClick: (event, rowData) => alert('You viewed ' + rowData.first_name)
-          }
-        ]}
-        icons={{
-          Search:       SearchIcon,
-          ResetSearch:  ClearIcon,
-          SortArrow:    ArrowDropUpIcon,
-          PreviousPage: ArrowLeftIcon,
-          NextPage:     ArrowRightIcon,
-          FirstPage:    SkipPreviousIcon,
-          LastPage:     SkipNextIcon
-        }}
-        tableLayout="fixed"
-        isLoading={isLoading}
-      />
+      <div style={{ height: 500, width: '100%' }} className={root}>
+        <div style={{ display: 'flex', height: '100%' }}>
+          <div style={{ flexGrow: 1 }}>
+            <DataGrid
+              autoHeight
+              columns={columns}
+              rows={staff}
+              disableColumnMenu
+              hideFooterSelectedRowCount
+              disableColumnSelector
+              onRowClick={({ row:{ email } }, e) => console.log({ email })}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
