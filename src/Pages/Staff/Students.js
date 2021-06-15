8072646df86c37a24f4df9c709021b18bc936eeb
Chigbogu Orji <brightorji60@gmail.com>
2021-06-16 00:27:10 +0100

    Style: Change container component to fix margin inconsistency

--- src/Pages/Staff/Students.js
+++ src/Pages/Staff/Students.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
-import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import StudentsTable from '../../Components/Staff/StudentsTable';
 import AdmissionForm from '../../Components/Staff/AdmissionForm';
 import StudentInfo from '../../Components/Staff/StudentInfo';
@@ -9,14 +9,14 @@ import NotFound from '../../Pages/404';
 const Students = () => {
   const match = useRouteMatch();
   return (
-    <Container>
+    <Grid item xs={12}>
       <Switch>
         <Route path={match.path} exact component={StudentsTable} />
         <Route path={`${match.path}/admission`} component={AdmissionForm} />
         <Route path={`${match.path}/info`} component={StudentInfo} />
         <Route component={NotFound} />
       </Switch>
-    </Container>
+    </Grid>
   );
 };
 
