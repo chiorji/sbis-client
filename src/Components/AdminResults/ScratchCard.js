c253e29e016d55ada5b748fb9de05f6f28c5f824
Chigbogu Orji <brightorji60@gmail.com>
2021-06-16 01:38:21 +0100

    Style: USe data-grid for tabular data

--- src/Components/AdminResults/ScratchCard.js
+++ src/Components/AdminResults/ScratchCard.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import Container from '@material-ui/core/Container';
+import { DataGrid } from '@material-ui/data-grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
@@ -10,17 +11,11 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import { makeStyles } from '@material-ui/styles';
-import MaterialTable from 'material-table';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import SearchIcon from '@material-ui/icons/Search';
-import ClearIcon from '@material-ui/icons/Clear';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
-import { getCards, generateCard, deleteCard } from '../../store/staff/actions';
+import { getCards, generateCard } from '../../store/staff/actions';
 
+/**
+ * @TODO: Remove the card delete feature, card should be auto-removed
+ */
 const useStyles = makeStyles(theme => ({
   gridItem: {
     display:        'flex',
@@ -37,7 +32,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const ScratchCard = ({ genCards, pins, getAllCard, isLoading, deleteCard }) => {
+const ScratchCard = ({ genCards, pins, getAllCard, isLoading }) => {
   const { gridItem, rowText, submitBtn } = useStyles();
   const [quantity, setQuantity] = useState(10);
 
@@ -50,8 +45,50 @@ const ScratchCard = ({ genCards, pins, getAllCard, isLoading, deleteCard }) => {
     getAllCard();
   }, [getAllCard]);
 
+  const columns = [
+    {
+      id:         1,
+      headerName: 'Pin',
+      field:      'pin',
+      width:      200,
+      flex:       1
+    },
+    {
+      id:         2,
+      headerName: 'Serial',
+      field:      'serial',
+      width:      200
+    },
+    {
+      id:         3,
+      headerName: 'Usage',
+      field:      'usage_count',
+      width:      200
+    },
+    {
+      id:             4,
+      headerName:     'Generated on',
+      field:          'created_at',
+      type:           'date',
+      width:          250,
+      valueFormatter: ({ value }) => {
+        return new Date(value).toDateString();
+      }
+    },
+    {
+      id:             5,
+      headerName:     'Expiration',
+      field:          'updated_at',
+      type:           'date',
+      width:          200,
+      valueFormatter: ({ value }) => {
+        return new Date(value).toDateString();
+      }
+    }
+  ];
+
   return (
-    <Container>
+    <>
       <Typography variant="h4">Scratch Card</Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} className={gridItem}>
@@ -67,7 +104,7 @@ const ScratchCard = ({ genCards, pins, getAllCard, isLoading, deleteCard }) => {
                   onChange={e => setQuantity(e.target.value)}
                   label="Quantity"
                 >
-                  {[ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
+                  {[20, 50, 80, 100 ]
                     .map(value => (
                       <MenuItem
                         key={value}
@@ -91,59 +128,31 @@ const ScratchCard = ({ genCards, pins, getAllCard, isLoading, deleteCard }) => {
           </form>
         </Grid>
         <Grid item xs={12}>
-          <MaterialTable
-            title={`Total: ${pins.length}`}
-            columns={[
-              { title: 'Pin.', field: 'pin' },
-              { title: 'Serial', field: 'serial' },
-              { title: 'Usage', field: 'usage_count' },
-              { title: 'Generated on', field: 'created_at', type: 'date', dateSetting: { locale: 'en-NG', format: 'dd/MM/yyyy' } },
-              { title: 'Expiration', field: 'updated_at', type: 'date', dateSetting: { locale: 'en-NG', format: 'dd/MM/yyyy' } }
-            ]}
-            options={{
-              search:              false,
-              sorting:             false,
-              actionsColumnIndex:  -1,
-              paging:              true,
-              pageSize:            10,
-              pageSizeOptions:     [10, 20, 50, 100],
-              debounceInterval:    600,
-              emptyRowsWhenPaging: false,
-              tableLayout:         'auto'
-            }}
-            data={pins}
-            actions={[
-              {
-                icon:    DeleteForeverIcon,
-                tooltip: 'Delete',
-                onClick: (e, rowData) => {
-                  deleteCard({ pin: rowData.pin, serial: rowData.serial });
-                }
-              }
-            ]}
-            icons={{
-              Search:       SearchIcon,
-              ResetSearch:  ClearIcon,
-              SortArrow:    ArrowDropUpIcon,
-              PreviousPage: ArrowLeftIcon,
-              NextPage:     ArrowRightIcon,
-              FirstPage:    SkipPreviousIcon,
-              LastPage:     SkipNextIcon
-            }}
-            isLoading={isLoading}
+          <DataGrid
+            columns={columns}
+            rows={pins}
+            loading={isLoading}
+            pageSize={10}
+            autoHeight
+            disableColumnMenu
+            hideFooterSelectedRowCount
+            disableColumnSelector
+            onRowClick={({ row: { pin, serial } }, e) => {
+              console.log({ pin, serial });
+            }
+            }
           />
         </Grid>
         <small>&copy; Brightside Technologies {new Date().getFullYear()}
           {'||'} All Right Reserved</small>
       </Grid>
-    </Container>
+    </>
   );
 };
 
 const mapDispatch = (dispatch) => ({
   genCards:   (qty) => dispatch(generateCard(qty)),
-  getAllCard: () => dispatch(getCards()),
-  deleteCard: (pin) => dispatch(deleteCard(pin))
+  getAllCard: () => dispatch(getCards())
 });
 
 const mapState = ({ staff }) => ({
