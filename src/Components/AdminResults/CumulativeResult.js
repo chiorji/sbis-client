f1bd4a79994e6c222e07322498bbb5042d6dab69
Chigbogu Orji <brightorji60@gmail.com>
2021-06-18 15:22:51 +0100

    Feat: Bootstrap result entry for varying result types

---
+++ src/Components/AdminResults/CumulativeResult.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const CumulativeResult = () => {
+  return (
+    <Typography variant="h6">Cumulative Result</Typography>
+  );
+};
+
+export default CumulativeResult;
