f0de1330ef8d3bc3d46bfb5f9886e9464cf73117
Chigbogu Orji <brightorji60@gmail.com>
2021-06-24 04:30:46 +0100

    Chore: Remove unused varaibles

--- src/store/result/actions.js
+++ src/store/result/actions.js
@@ -1,63 +1,6 @@
 import types from './constants';
 
-export const validatePin = () => ({
-  type: types.VALIDATE_PIN_REQUEST
-});
-
-export const validatePinSuccess = (payload) => {
-  return dispatch => {
-    dispatch({
-      type: types.VALIDATE_PIN_SUCCESS,
-      payload
-    });
-  };
-};
-
-export const validatePinError = (payload) => {
-  return dispatch => {
-    dispatch({
-      type: types.VALIDATE_PIN_FAILURE,
-      payload
-    });
-  };
-};
-
-export const validateRegnoSuccess = (payload) => {
-  return dispatch => {
-    dispatch({
-      type: types.VALIDATE_REGNO_SUCCESS,
-      payload
-    });
-  };
-};
-
-export const validateRegnoFailure = (payload) => {
-  return dispatch => {
-    dispatch({
-      type: types.VALIDATE_REGNO_FAILURE,
-      payload
-    });
-  };
-};
-
-export const getResultSuccess = (payload) => {
-  return dispatch => {
-    dispatch({
-      type: types.GET_RESULT_SUCCESS,
-      payload
-    });
-  };
-};
-
-export const getResultFailure = (payload) => {
-  return dispatch => {
-    dispatch({
-      type: types.GET_RESULT_FAILURE,
-      payload
-    });
-  };
-};
-
+export const getResultRequest = () => ({ type: types.GET_RESULT_REQUEST });
 
 export const showAlert = (payload) => ({
   type: types.SHOW_RESULT_ALERT,
